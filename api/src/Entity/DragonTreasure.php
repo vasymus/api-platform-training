86c27465cfc8aeb42ed4d7822821dd9716b244c1
vasymus <vasymuszchd@mail.ru>
2024-09-13 20:59:33 +0100

    testing with dragon treasure entity and api serialization / deserialization

--- api/src/Entity/DragonTreasure.php
+++ api/src/Entity/DragonTreasure.php
@@ -4,37 +4,74 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\DragonTreasureRepository;
+use Carbon\Carbon;
+use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Serializer\Attribute\SerializedName;
 
 #[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    shortName: 'Treasure',
+    description: "A rare and valuable treasure.",
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Patch(),
+//        new Delete(),
+    ],
+    normalizationContext: [
+        'groups' => ['treasure:read'],
+    ],
+    denormalizationContext: [
+        'groups' => ['treasure:write']
+    ]
+)]
 class DragonTreasure
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups('treasure:read')]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['treasure:read', 'treasure:write'])]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups('treasure:read')]
     private ?string $description = null;
 
     #[ORM\Column]
     #[ApiProperty(description: "The estimated value of this treasure, in gold coins")]
+    #[Groups(['treasure:read', 'treasure:write'])]
     private ?int $value = null;
 
     #[ORM\Column]
+    #[Groups(['treasure:read', 'treasure:write'])]
     private ?int $coolFactor = null;
 
     #[ORM\Column]
-    private ?\DateTimeImmutable $createdAt = null;
+    private ?DateTimeImmutable $plunderedAt;
 
     #[ORM\Column]
-    private ?bool $isPublished = null;
+    private bool $isPublished = false;
+
+    public function __construct()
+    {
+        $this->plunderedAt = new DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
@@ -65,6 +102,15 @@ class DragonTreasure
         return $this;
     }
 
+    #[Groups(['treasure:write'])]
+    #[SerializedName('description')]
+    public function setTextDescription(string $description): static
+    {
+        $this->description = nl2br($description);
+
+        return $this;
+    }
+
     public function getValue(): ?int
     {
         return $this->value;
@@ -89,14 +135,21 @@ class DragonTreasure
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeImmutable
+    public function getPlunderedAt(): ?DateTimeImmutable
     {
-        return $this->createdAt;
+        return $this->plunderedAt;
     }
 
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    #[ApiProperty(description: 'A human-readable representation of when this treasure was plundered.')]
+    #[Groups(['treasure:read'])]
+    public function getPlunderedAtAgo(): string
     {
-        $this->createdAt = $createdAt;
+        return Carbon::instance($this->plunderedAt)->diffForHumans();
+    }
+
+    public function setPlunderedAt(DateTimeImmutable $plunderedAt): static
+    {
+        $this->plunderedAt = $plunderedAt;
 
         return $this;
     }
